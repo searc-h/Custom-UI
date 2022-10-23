6a262065691c07218142e6b8d45b7378fe5cb160
searc-h <2583448925@qq.com>
2022-10-23 13:40:13 +0800

    add the BGslider component

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,5 @@
+
+// less文件是 { [key:string] : string }类型的只读类对象
 declare module '*.module.less' {
     const classes: {
       readonly [key: string]: string
@@ -6,11 +8,13 @@ declare module '*.module.less' {
     
 }
 
+// JPG文件是 src的字符串类型变量
 declare module '*.jpg' {
   const src: string;
   export default src;
 }
 
+// 同理
 declare module '*.jpeg' {
   const src: string;
   export default src;
--- src/ui-components/index.ts
+++ src/ui-components/index.ts
@@ -2,9 +2,11 @@ import Button from "./Button/Button";
 import SwithCard from "./SwithCards/SwithCard";
 import Progress from "./Progress/Progress";
 import SoundBtn from "./SoundBoard/SoundBtn";
+import Slider from "./Silder/Slider";
 export {
     Button,
     SwithCard,
     Progress,
-    SoundBtn
+    SoundBtn,
+    Slider
 }
